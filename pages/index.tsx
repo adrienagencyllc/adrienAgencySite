5823b0b27f090b706547d9e139a248b712b1c296
Hernan Valencia <frjr17@criptext.com>
2023-05-11 01:16:14 -0500

    Adding responsive web design and new complete logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Divider, HStack, Text, VStack } from "@chakra-ui/react";
+import { Divider, Stack, Text, VStack } from "@chakra-ui/react";
 import Image from "next/image";
 export default function Home() {
   return (
@@ -10,18 +10,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HStack background={"black"} height={"100vh"} justify={"center"} spacing={30}>
-        <Image src={"/slogo-white.png"} width={100} height={100} alt={"Adrien Agency Simplified Logo"} />
-        <Divider orientation="vertical" height={40} />
-        <VStack width={"30%"}>
-          <Text fontStyle={"italic"} fontSize={"2xl"} width={"100%"}>
-            We&apos;re in building process right now, to bring you the best experience posible
+      <Stack direction={{ base: "column", md: "row" }} background={"black"} height={"100vh"} align={"center"} justify={"center"} spacing={30}>
+        <Image src={"/logo-white.png"} width={300} height={300} alt={"Adrien Agency Simplified Logo"} />
+        <Divider display={{ base: "none", md: "block" }} orientation="vertical" height={40} />
+        <VStack width={{ base: "90%", md: "30%" }}>
+          <Text fontStyle={"italic"} textAlign={{ base: "center", md: "left" }} fontSize={"2xl"} width={"100%"}>
+            We&apos;re in building process right now, to bring you the best experience posible.
           </Text>
-          <Text fontStyle={"italic"} fontSize={"2xl"} width={"100%"}>
+          <Text fontStyle={"italic"} textAlign={{ base: "center", md: "left" }} fontSize={"2xl"} width={"100%"}>
             But don&apos;t be sad, we&apos;ll be here soon...
           </Text>
         </VStack>
-      </HStack>
+      </Stack>
     </>
   );
 }
